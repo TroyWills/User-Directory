a172d5eeddde92115ed387cd007561b6a5e5ccab
Troy Wills <troy_wills@outlook.com>
2021-03-25 17:35:44 -0500

    start

---
+++ src/components/SearchBox.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/SearchBox.css"
+
+function SearchBox() {
+
+    return (
+        <div class="form-outline">
+            <input type="search" 
+            id="form1" 
+            class="form-control" 
+            placeholder="Search"
+            />
+        </div>
+    )
+
+
+}
+
+export default SearchBox;
