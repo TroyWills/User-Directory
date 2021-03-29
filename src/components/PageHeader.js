9d385a34d32bfe0df12ce5b885a646ebd33a7bd2
Troy Wills <troy_wills@outlook.com>
2021-03-28 23:35:35 -0500

    try again

---
+++ src/components/PageHeader.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/PageHeader.css"
+
+function PageHeader() {
+
+    return (
+            <h1 class="display-4">Employee Directory</h1>
+    )
+}
+
+export default PageHeader;
