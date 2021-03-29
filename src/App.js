10ec34a08fa289a9bdcb8d9ed5e6b5066f57f556
Troy Wills <troy_wills@outlook.com>
2021-03-29 00:29:48 -0500

    save

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import PageHeader from "./components/PageHeader";
+import SearchBox from "./components/Body";
+import Body from './components/Body';
+
+function App() {
+  return (
+
+<div>
+<PageHeader />
+<Body />
+</div>
+
+  );
+}
+
+export default App;
